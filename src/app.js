c12d136fcb79f1e77af9b02f1b7d8579a4e24395
VinaKale1998 <vinaykale1999@gmail.com>
2023-11-11 14:20:42 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ import bodyParser from "body-parser";
 import { Server } from "socket.io";
 import http from "http";
 import { rejects } from "assert";
-import { addUser,newUser,getIndividualGroupUsers, users } from "./helpers/helper.js";
+import {
+  addUser,
+  newUser,
+  getIndividualGroupUsers,
+  users,
+} from "./helpers/helper.js";
 
+import AWS from "aws-sdk";
+
+AWS.config;
 
 const app = express();
 
@@ -25,41 +33,31 @@ io.on("connection", (socket) => {
   console.log(`Socket is connected to ${socket.id}`);
 
   socket.on("login", (data) => {
-    console.log("login event received",data)
-    if (!addUser(data)) 
-    {
-      io.emit("loginError", { message: "User already present" })
-    
+    console.log("login event received", data);
+    if (!addUser(data)) {
+      io.emit("loginError", { message: "User already present" });
+    } else {
+      const user = newUser(socket.id, data.userName, data.group);
+      socket.join(user.group);
+      io.emit("loggedIn", { message: "user logged in", user: user });
+      socket.broadcast.to(user.group).emit("entered", {
+        message: `${user.userName} has entered the chat`,
+        group: user.group,
+        userName: "Admin",
+      });
+
+      io.to(user.group).emit("groupUsers", getIndividualGroupUsers(user.group));
     }
-  else
-    {
-      const user= newUser(socket.id,data.userName,data.group);
-      socket.join(user.group)
-      io.emit("loggedIn", { message: "user logged in" ,user:user})
-      socket.broadcast.to(user.group).emit("entered",{message:`${user.userName} has entered the chat`})
-      
-      io.to(user.group).emit("groupUsers",getIndividualGroupUsers(user.group));
-     
-    }
-    
-  }
-  
-  
-  );
+  });
 
-  socket.on("message",(data)=>{
-    console.log("message received is ", data.message)
+  socket.on("message", (data) => {
+    console.log("message received is ", data.message);
 
-    io.to(data.group).emit("message",data)
-  })
-
-
- 
-
-
+    io.to(data.group).emit("message", data);
+  });
 });
 
-const PORT = process.env.PORT || 4000;  
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, (err) => {
   console.log("Server is running");
